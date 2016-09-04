28d7d08c3c1014dfccbe881d434b0dd26656e31e
Brunno Vodola Martins <bvodola@MacBook-Air-de-Brunno.local>
2016-09-04 16:45:16 -0300

    Inserted Access-Control-Allow-Methods

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ var port = (process.env.HOSTNAME == 'web506.webfaction.com' ? 18972 : 4000);
 
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
